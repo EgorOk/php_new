a3fe9a9e0b24227e191d4e5d877e809f54d7a066
EgorOk <egorcud33@gmail.com>
2022-02-15 22:50:56 +0300

    исправление бага с неверным расчета id, и дублированием при удаление одной темы

--- forum_server.php
+++ forum_server.php
@@ -13,11 +13,9 @@ $db_connect = mysqli_connect('localhost', 'mysql', 'mysql', "login_email_passwor
     $preview = $_POST["preview"];
     $author = $_SESSION['login'];
     $date = date('m/d/Y h:i:s a', time());
+    $res = mysqli_query($db_connect, 'SELECT topic_id FROM forum_topic');
 
-    $qwe = "SELECT COUNT(1) FROM forum_topic";
-    $resultat = $db_connect->query($qwe);
-    $id = $resultat->fetch_array(MYSQLI_NUM);
-    $id_sum = $id[0] + 1;
+    $id_sum = shetId($res);
 
     $query = "INSERT INTO `forum_topic`(topic_id, topic_name, topic_preview, topic_author, time) VALUES ('$id_sum', '$name', '$preview', '$author', 'now()')";
     if (!mysqli_query($db_connect, $query)) {
@@ -37,11 +35,11 @@ $time = array_empty($time);
 $string_forum = 0;
 
 while ($row = $res->fetch_array()) {
-    $topic_id[] = $row[0];
-    $topic_name[] = $row[1];
-    $topic_preview[] = $row[2];
-    $topic_author[] = $row[3];
-    $time[] = $row[4];
+    $topic_id[$string_forum] = $row[0];
+    $topic_name[$string_forum] = $row[1];
+    $topic_preview[$string_forum] = $row[2];
+    $topic_author[$string_forum] = $row[3];
+    $time[$string_forum] = $row[4];
     $string_forum++;
 }
 if (isset($topic_name)) {
@@ -59,5 +57,23 @@ header('Location: forum.php');
 function array_empty()
 {
     return array();
+}
+
+function shetId($res)
+{
+    $shet_id = array_empty();
+    $string_id = 0;
+
+    while ($resultat = $res->fetch_array()) {
+        $shet_id[$string_id] = $resultat[0];
+        $string_id++;
+    }
+    array_multisort($shet_id);
+    $shet = 1;
+
+    while ($shet == $shet_id[$shet - 1]) {
+        $shet++;
+    }
+    return $shet;
 }
 ?>
